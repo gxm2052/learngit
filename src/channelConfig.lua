5a70b36ceb8c7b6a448756996456380ee6ee0a75
gong.xiaoming <gxm2052@163.com>
2015-10-29 11:31:10 +0800

    add files 2

---
+++ src/channelConfig.lua
@@ -0,0 +1,6 @@
+
+CHANNEL = 0                     -- 0 IOS
+DEFAULT_VERSION = "1.0.0"       --版本号
+IP = "125.88.152.21"          --正式服
+USE_DATAEYE = false             --是否使用DataEye数据统计
+USE_SMS_PAY = false             --是否使用短代支付
